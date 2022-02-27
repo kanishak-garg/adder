06a112c4e1fd939f0ee1cdea58ada1b9ebc684ee
kanishak garg <kanishakgarg8778@gmail.com>
2022-02-27 15:51:09 +0530

    change ol start number

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var inp = document.getElementById("inp");
 var one = document.getElementById("one");
 var two = document.getElementById("two");
 var total = document.getElementById("sum");
+var start = document.getElementById("start");
 
 var sum = 0;
 function processForm(e) {
@@ -22,7 +23,6 @@ function processForm(e) {
   li.appendChild(document.createTextNode(sum));
   two.appendChild(li);
 
-  console.log(sum);
   inp.value = "";
   return false;
 }
@@ -32,4 +32,20 @@ if (form.attachEvent) {
   form.attachEvent("submit", processForm);
 } else {
   form.addEventListener("submit", processForm);
+}
+
+function processForm1(e) {
+  if (e.preventDefault) e.preventDefault();
+  if (start.value == "") {
+    return false;
+  }
+  one.setAttribute("start", start.value);
+  return false;
+}
+
+var form = document.getElementById("reciet");
+if (form.attachEvent) {
+  form.attachEvent("submit", processForm1);
+} else {
+  form.addEventListener("submit", processForm1);
 }
